f0165ff69c4884119597666e2527ea48fc854deb
sebastiann88 <sebastian.nadeau45@gmail.com>
2022-11-23 21:08:07 -0500

    refactor(all): remove code

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
-// --------- Variables
-
-// ---- Greensock
-
 const html = document.documentElement;
 const canvas = document.getElementById("hero-lightpass");
 const context = canvas.getContext("2d");
@@ -18,14 +14,10 @@ const preloadImages = () => {
   }
 };
 
-// ---- Canvas
-const canvasViewport = document.querySelector('canvas.webgl')
-
 const img = new Image()
 img.src = currentFrame(1);
-canvas.width=2560;
-canvas.height=1921;
-// canvas: canvasViewport;
+canvas.width=1158;
+canvas.height=869;
 img.onload=function(){
   context.drawImage(img, 0, 0);
 }
@@ -34,41 +26,6 @@ const updateImage = index => {
   img.src = currentFrame(index);
   context.drawImage(img, 0, 0);
 }
-
-// ---- ThreeJS
-
-/**
- * Sizes
- */
-const sizes = {
-    width: window.innerWidth,
-    height: window.innerHeight
-}
-
-/**
- * Camera
- */
-// Base camera
-// const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
-// camera.position.z = 3
-// scene.add(camera)
-
-// Controls
-// const controls = new OrbitControls(camera, canvas)
-// controls.enableDamping = true
-
-/**
- * Renderer
- */
-// const renderer = new THREE.WebGLRenderer({
-//     canvas: canvas
-// })
-// renderer.setSize(sizes.width, sizes.height)
-// renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
-
-// --------- Main Code
-
-// ---- GreenSock
 
 window.addEventListener('scroll', () => {  
   const scrollTop = html.scrollTop;
@@ -83,20 +40,3 @@ window.addEventListener('scroll', () => {
 });
 
 preloadImages()
-
-// ---- Three JS
-
-window.addEventListener('resize', () =>
-{
-    // Update sizes
-    sizes.width = window.innerWidth
-    sizes.height = window.innerHeight
-
-    // Update camera
-    // camera.aspect = sizes.width / sizes.height
-    // camera.updateProjectionMatrix()
-
-    // Update renderer
-    // renderer.setSize(sizes.width, sizes.height)
-    // renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
-})
